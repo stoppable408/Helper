38e8f52bad4f18aa9eafe3324b85d1ee7e28b10d
Lennon Turner <Lennon.Turner@carat.com>
2019-02-01 13:50:14 -0500

    removing unnecessary files

--- test_calendar.py
+++
@@ -1,8 +0,0 @@
-import os
-import random
-os.chdir(os.getcwd()+r"\assets")
-print(random.choice(os.listdir()))
-directory = os.getcwd()+r"\assets"
-# for gif in directory:
-    # print(gif)
-            
